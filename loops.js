c50c6e9a7dd0ba24c7163c764017ab25a3f162b9
JoanneOs <jowan.awsman@yahoo.com>
2025-02-27 18:39:43 -0600

    if else

--- loops.js
+++ loops.js
@@ -3,13 +3,20 @@ console.log("Hi, this is ALAB 308.3.1: Practical Loops");
 // Part 1: Fizz Buzz
 //Loop through all numbers from 1 to 100.
 console.log("Loop through all numbers from 1 to 100.");
-for (let i=1;i<=100;i++){
-    {console.log(i);}
 
-// If a number is divisible by 3, log “Fizz.”
-//i=55;
-//console.log("If a number is divisible by 3, log “Fizz.");
-if(i%3==0){console.log(i+" fizz")}
-if(i%5==0){console.log(i+" buzz")}
-if((i%3==0)&&(i%5==0)){console.log(i+" fizz buzz")}
+for (let i = 1; i <= 100; i++) {
+  //     {console.log(i);}
+
+  // If a number is divisible by 3, log “Fizz.”
+  //i=55;
+  //console.log("If a number is divisible by 3, log “Fizz.");
+  if (i % 3 == 0) {
+    console.log(" fizz");
+  }else if (i % 5 == 0) {
+    console.log(" buzz");
+  }else if (i % 3 == 0 && i % 5 == 0) {
+    console.log(" fizz buzz");
+  } else {
+    console.log(i);
+  }
 }
