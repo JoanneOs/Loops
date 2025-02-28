05ec9bf5e0de71ef963ac56772899af4fe7c218f
JoanneOs <jowan.awsman@yahoo.com>
2025-02-27 18:23:41 -0600

    1-100

--- loops.js
+++ loops.js
@@ -1,1 +1,7 @@
 console.log("Hi, this is ALAB 308.3.1: Practical Loops");
+
+// Part 1: Fizz Buzz
+//Loop through all numbers from 1 to 100.
+console.log("Loop through all numbers from 1 to 100.");
+for (let i=1;i<=100;i++)
+    {console.log(i);}
